c8044964aa0f9686de384ad38e24efde1dfa4767
Chase Stevens <chase.stevens@hgs.hiddengeniusproject.org>
2023-01-25 17:54:59 -0800

    first commit

---
+++ Python/Madlibs/README.md
@@ -0,0 +1,8 @@
+Output: The reciever for input (your code.) - It displays the output of your code
+String (data Type): A text variable 
+string literal: A multi line string
+String Concatenation (different ways): How string variables can be connected.
+fString: A type of string concatenation
+variables: Something that data 
+input: Something from a user that gets transproted to the output
+Comments: Lines of code the computer doesnt read
---
+++ Python/Madlibs/mablibs.py
@@ -0,0 +1,20 @@
+# String concatenation (How string can be connected)
+# A string, 'Subscribe to _____'
+youtuber = "BestYoutuberFTW1337" # A string variable that can be called
+
+# Multiple solutions
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#rint(f"subscribe to {youtuber}")
+
+adj                     = input("Adjective: ")
+verb1                   = input("Verb: ")
+verb2                   = input("Verb: ")
+famous_person           = input("Famous individual: ")
+
+
+madlib = f"Computer programming is so {adj}! it makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!" 
+
+print(madlib)
+
